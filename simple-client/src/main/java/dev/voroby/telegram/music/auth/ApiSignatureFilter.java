09ecc20de161cbdf1ce5d084b717bce66877d42c
cengt <cengt@mail.com>
2026-02-13 14:07:23 +0800

    remove api auth path from verify

--- simple-client/src/main/java/dev/voroby/telegram/music/auth/ApiSignatureFilter.java
+++ simple-client/src/main/java/dev/voroby/telegram/music/auth/ApiSignatureFilter.java
@@ -44,6 +44,12 @@ public class ApiSignatureFilter extends OncePerRequestFilter {
     }
 
     @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
+        String path = request.getServletPath();
+        return path.startsWith("/api/authorization");
+    }
+
+    @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
 
